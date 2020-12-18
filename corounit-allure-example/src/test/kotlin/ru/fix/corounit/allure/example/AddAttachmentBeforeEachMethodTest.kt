429ea992f7c05b0af8ce511a7eee41a7968cf19b
vkuznetsov <vkuznetsov@fix.ru>
2020-12-18 15:47:04 +0300

    add test example

--- corounit-allure-example/src/test/kotlin/ru/fix/corounit/allure/example/AddAttachmentBeforeEachMethodTest.kt
+++ corounit-allure-example/src/test/kotlin/ru/fix/corounit/allure/example/AddAttachmentBeforeEachMethodTest.kt
@@ -3,13 +3,24 @@ package ru.fix.corounit.allure.example
 import io.kotlintest.matchers.boolean.shouldBeTrue
 import mu.KotlinLogging
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import ru.fix.corounit.allure.AllureStep
 import ru.fix.corounit.allure.invoke
 
 private val log = KotlinLogging.logger { }
 
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AddAttachmentBeforeEachMethodTest {
+
+    suspend fun beforeAll() {
+        /**
+         * Вот тут при получении по ключу сваливаемся,
+         * ru.fix.corounit.allure.AllureStep$Companion.fromCurrentCoroutineContext(AllureStep.kt:40)
+         * т.к. элемента по кючу еще нет в coroutineContext
+         */
+        AllureStep.attachment("attach", "content")
+    }
 
     suspend fun beforeEach() {
         AllureStep.attachment("attach", "content")
